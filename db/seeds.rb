3a3e552ce2fa1cff2c6da8150cd62fb13f78221a
Hamdija Halilovic <hamdija_hh@hotmail.com>
2020-11-11 16:54:24 +0100

    Challenge 3 in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Munich Towers',
+  address: 'Hauptstr',
+  description: 'Great place',
+  price_per_night: 15,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Melbourne Heights',
+  address: 'Main street',
+  description: 'Awesome place to stay',
+  price_per_night: 105,
+  number_of_guests: 20
+)
+
+Flat.create!(
+  name: 'Berlin Hotel',
+  address: 'Main road',
+  description: 'cozy hotel',
+  price_per_night: 25,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Hamburg',
+  address: 'Hamburg str',
+  description: 'medium sized hotel',
+  price_per_night: 55,
+  number_of_guests: 15
+)
